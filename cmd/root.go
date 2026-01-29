80e6422e81a7f9cd15c92a30230a3661734dc742
Matt Krueger <mkrueger@rstms.net>
2026-01-28 21:28:08 -0700

    Bump version: 0.1.0 → 1.0.0

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.1.0",
+	Version: "1.0.0",
 	Use:     "fastcgid",
 	Short:   "FastCGI TCP server",
 	Long:    `Listening TCP FastCGI Server`,
